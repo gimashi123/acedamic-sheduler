42974d93341ac2712ebfbe384533e522109530bc
chamindu1226 <154747732+chamindu1226@users.noreply.github.com>
2025-03-04 13:26:49 +0530

    Create database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import {connect_db} from "./config/db.config.js";
+
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors({origin:true,credentials:true}));
-
-
-
-
 
 const PORT = process.env.BACKEND_PORT;
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
 
+await connect_db().then(()=> {
+    console.log('connected to db')
+});
+
 app.get('/hello',(_,res)=>{
     res.send('backend is up and running')
 })
+
+
 
