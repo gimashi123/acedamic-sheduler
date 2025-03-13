1d3f4712bc9a64643ff8af512ad21574ef572775
RavinduPerera <it22064622@my.sliit.lk>
2025-03-13 12:03:27 +0530

    made some changes on the server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connect_db } from './config/db.config.js';
+
 import authRoute from './routes/auth.route.js';
 import userRoute from './routes/user.route.js';
+import groupRoutes from './routes/group.route.js';
+import venueRoutes from './routes/venue.route.js';
 
 const app = express();
 dotenv.config();
@@ -26,3 +29,5 @@ app.get('/hello', (_, res) => {
 
 app.use('/auth', authRoute);
 app.use('/user', userRoute);
+app.use('/api/groups', groupRoutes);
+app.use('/api/venues', venueRoutes);
