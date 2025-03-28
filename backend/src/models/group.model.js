d3134a33be68f8daf2df04350d6da50483c3b23d
RavinduPerera <it22064622@my.sliit.lk>
2025-03-28 14:08:25 +0530

    removed 3 fileds from backend (faculty, semester, group type)

--- backend/src/models/group.model.js
+++ backend/src/models/group.model.js
@@ -7,10 +7,10 @@ const groupSchema = new mongoose.Schema ({
         required: true
     },
 
-    faculty: {
-        type: String,
-        required: true
-    },
+    // faculty: {
+    //     type: String,
+    //     required: true
+    // },
 
     department: {
         type: String,
@@ -22,28 +22,28 @@ const groupSchema = new mongoose.Schema ({
         required: true
     },
 
-    semester: {
-        type: Number, 
-        required: true
-    },
+    // semester: {
+    //     type: Number, 
+    //     required: true
+    // },
 
-    groupType: {
-        type: String,
-        enum: ['weekday', 'weekend'],
-        required: true,
-    },
+    // groupType: {
+    //     type: String,
+    //     enum: ['weekday', 'weekend'],
+    //     required: true,
+    // },
 
     students: {
         type: [String], // Store student IDs or names
-        validate: [studentsLimit, 'A group cannot have more than 60 students'],
+        validate: [studentsLimit, 'A group cannot have more than 20 students'], // reduced limit to 20
         default: []
     }
 
 }, {timestamps: true});
 
-// function to filter students 
+// function to filter students - reduced limit to 20
 function studentsLimit(val) {
-    return val.length <= 60
+    return val.length <= 20
 }
 
 const Group = mongoose.model('Group', groupSchema);
